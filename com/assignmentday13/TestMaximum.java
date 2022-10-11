4c5953b9dae3cf575307ba9b70474f9b83f4059a
Shiv25z <shivamzagade25899@gmail.com>
2022-10-11 23:07:13 +0530

    Added: Refractor & Generics Class with Parameterized Constructor

--- com/assignmentday13/TestMaximum.java
+++ com/assignmentday13/TestMaximum.java
@@ -1,32 +1,43 @@
 package com.assignmentday13;
 
-import java.util.Scanner;
 
-public class TestMaximum {
-    public <T extends Comparable<T>> T compareTo(T a,T b,T c) {// UC4
-        if(a.compareTo(b) > 0){
-            if(a.compareTo(c) > 0) {
-                return a;
+
+public class TestMaximum <T extends Comparable<T>> {
+
+        T a,b,c;
+        //Generic Constructor for Handling Different Type Object
+        TestMaximum(T a, T b, T c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        //compareTo method for Checking Maximum from 3 Integer Object
+        public T compareTo() {
+            if (a.compareTo(b) > 0) {
+                if (a.compareTo(c) > 0) {
+                    return a;
+                } else {
+                    return c;
+                }
             } else {
-                return c;
-            }
-        } else {
-            if(b.compareTo(c) > 0) {
-                return b;
-            } else {
-                return c;
+                if (b.compareTo(c) > 0) {
+                    return b;
+                } else {
+                    return c;
+                }
             }
         }
-    }
+
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Test Maximum Problem!");
-        TestMaximum integerComparison = new TestMaximum();
-        System.out.println("Maximum from 3 Integer: " +integerComparison.compareTo(19, 12, 20));
-        TestMaximum floatComparison = new TestMaximum();
-        System.out.println("Maximum from 3 Float : " +floatComparison.compareTo(19.0F, 12.0F, 20.0F));
-        TestMaximum stringComparison = new TestMaximum();
-        System.out.println("Maximum from 3 String : " +stringComparison.compareTo("Apple", "Peach", "Banana"));
+        TestMaximum<Integer> integerComparison = new TestMaximum(19, 12, 20);
+        System.out.println("Maximum from 3 Integer: " +integerComparison.compareTo());
+        TestMaximum<Float> floatComparison = new TestMaximum(19.0, 12.0, 20.0);
+        System.out.println("Maximum from 3 Float : "+floatComparison.compareTo());
+        TestMaximum<String> stringComparison = new TestMaximum("Apple", "Peach", "Banana");
+        System.out.println("Maximum from 3 String : " +stringComparison.compareTo());
     }
 }
