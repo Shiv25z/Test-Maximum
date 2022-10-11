5a200c33fb46b26ba6f9542212876c0f6d7582a9
Shiv25z <shivamzagade25899@gmail.com>
2022-10-11 21:59:22 +0530

    Added: Output Maximum of 3 Integers

--- com/assignmentday13/TestMaximum.java
+++ com/assignmentday13/TestMaximum.java
@@ -1,9 +1,29 @@
 package com.assignmentday13;
 
+import java.util.Scanner;
+
 public class TestMaximum {
+    static void maxInt(int a, int b, int c){
+        if(a==b||b==c||a==c){
+            System.out.println("Two same numbers entered");
+        }
+        if(a>b&&a>c)
+            System.out.println("Maximum : "+a);
+        if(b>a&&b>c)
+            System.out.println("Maximum : "+b);
+        else{
+            System.out.println("Maximum : "+c);
+        }
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Test Maximum Problem!");
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 Integers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        maxInt(a,b,c);// UC1
     }
 }
