953e3fe7d3fd51a8b16c9ed921d529f83b459e03
Shiv25z <shivamzagade25899@gmail.com>
2022-10-11 22:17:36 +0530

    Added: Output Maximum of 3 Float

--- com/assignmentday13/TestMaximum.java
+++ com/assignmentday13/TestMaximum.java
@@ -15,15 +15,28 @@ public class TestMaximum {
             System.out.println("Maximum : "+c);
         }
     }
+    static void maxFloat(float a, float b, float c){
+        if(a==b||b==c||a==c){
+            System.out.println("Two same numbers entered");
+        }
+        if(a>b&&a>c)
+            System.out.println("Maximum : "+a);
+        if(b>a&&b>c)
+            System.out.println("Maximum : "+b);
+        else{
+            System.out.println("Maximum : "+c);
+        }
+    }
 
 
     public static void main(String[] args) {
         System.out.println("Welcome to Test Maximum Problem!");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 3 Integers");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        maxInt(a,b,c);// UC1
+        float a = sc.nextInt();
+        float b = sc.nextInt();
+        float c = sc.nextInt();
+        maxInt((int) a,(int)b,(int)c);// UC1
+        maxFloat(a, b, c);// UC2
     }
 }
